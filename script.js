75bc5f1927ef7c1737b6f2678a5e9e757d79780c
Miguel Domingos <migueldsp@hotmail.com>
2022-08-11 21:45:24 -0300

    deselect item with onclick

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const trackMainCourses = document.getElementById("mainCourse");
-const mainCourses = Array.from(trackMainCourses.children);
-const trackBeverages = document.getElementById("beverage");
-const beverages = Array.from(trackBeverages.children);
-const trackDesserts = document.getElementById("dessert");
-const desserts = Array.from(trackDesserts.children);
+const trackMainCourses = document.getElementById("mainCourses");
+const mainCoursesArray = Array.from(trackMainCourses.children);
+const trackBeverages = document.getElementById("beverages");
+const beveragesArray = Array.from(trackBeverages.children);
+const trackDesserts = document.getElementById("desserts");
+const dessertsArray = Array.from(trackDesserts.children);
 
 const order = [];
 order.cellPhone = "+5522992175627";
@@ -12,64 +12,127 @@ const borderColorChanger = (current, target) => {
      current.classList.remove("current");
      target.classList.add("current");
 };
-trackMainCourses.addEventListener("click", (e) => {
-     const currentMainCourse = trackMainCourses.querySelector(".current");
-     if (currentMainCourse) {
-          currentMainCourse.classList.remove("current");
-     }
-     let targetIndex = mainCourses.indexOf(e.target);
-     if (targetIndex != -1) {
-          mainCourses[targetIndex].classList.add("current");
-     } else if (mainCourses.indexOf(e.target.parentElement) != -1) {
-          targetIndex = mainCourses.indexOf(e.target.parentElement);
-          mainCourses[targetIndex].classList.add("current");
-     }
-     order.mainCourse = mainCourses[targetIndex].children[1].innerText;
-     order.mainCoursePrice = Number(
-          mainCourses[targetIndex].children[3].innerText.replace(",", ".").replace("R$ ", "")
-     );
-     buttonChange();
-});
-trackBeverages.addEventListener("click", (e) => {
-     const targetBeverages = trackBeverages.querySelector(".current");
-     if (targetBeverages) {
-          targetBeverages.classList.remove("current");
-     }
-     let targetIndex = beverages.indexOf(e.target);
-     if (targetIndex != -1) {
-          beverages[targetIndex].classList.add("current");
-     } else if (beverages.indexOf(e.target.parentElement) != -1) {
-          targetIndex = beverages.indexOf(e.target.parentElement);
-          beverages[targetIndex].classList.add("current");
-     }
-     order.beverage = beverages[targetIndex].children[1].innerText;
-     order.beveragePrice = Number(
-          beverages[targetIndex].children[3].innerText.replace(",", ".").replace("R$ ", "")
-     );
-     buttonChange();
-});
-trackDesserts.addEventListener("click", (e) => {
-     const currentDesserts = trackDesserts.querySelector(".current");
-     if (currentDesserts) {
-          currentDesserts.classList.remove("current");
-     }
-     let targetIndex = desserts.indexOf(e.target);
-     if (targetIndex != -1) {
-          desserts[targetIndex].classList.add("current");
-     } else if (desserts.indexOf(e.target.parentElement) != -1) {
-          targetIndex = desserts.indexOf(e.target.parentElement);
-          desserts[targetIndex].classList.add("current");
-     }
-     order.dessert = desserts[targetIndex].children[1].innerText;
-     order.dessertPrice = Number(
-          desserts[targetIndex].children[3].innerText.replace(",", ".").replace("R$ ", "")
-     );
-     buttonChange();
-});
+
+const menu = {
+     mainCourses(targetId) {
+          const currentSelection = trackMainCourses.querySelector(".current");
+          if (currentSelection) {
+               const currentId = mainCoursesArray.indexOf(currentSelection);
+               if (currentId == targetId - 1) {
+                    mainCoursesArray[targetId - 1].classList.remove("current");
+                    buttonChange();
+               } else {
+                    mainCoursesArray[currentId].classList.remove("current");
+                    mainCoursesArray[targetId - 1].classList.add("current");
+                    buttonChange();
+               }
+          } else {
+               mainCoursesArray[targetId - 1].classList.add("current");
+               buttonChange();
+          }
+     },
+     beverages(targetId) {
+          const currentSelection = trackBeverages.querySelector(".current");
+          if (currentSelection) {
+               const currentId = beveragesArray.indexOf(currentSelection);
+               if (currentId == targetId - 1) {
+                    beveragesArray[targetId - 1].classList.remove("current");
+                    buttonChange();
+               } else {
+                    beveragesArray[currentId].classList.remove("current");
+                    beveragesArray[targetId - 1].classList.add("current");
+                    buttonChange();
+               }
+          } else {
+               beveragesArray[targetId - 1].classList.add("current");
+               buttonChange();
+          }
+     },
+     desserts(targetId) {
+          const currentSelection = trackDesserts.querySelector(".current");
+          if (currentSelection) {
+               const currentId = dessertsArray.indexOf(currentSelection);
+               if (currentId == targetId - 1) {
+                    dessertsArray[targetId - 1].classList.remove("current");
+                    buttonChange();
+               } else {
+                    dessertsArray[currentId].classList.remove("current");
+                    dessertsArray[targetId - 1].classList.add("current");
+                    buttonChange();
+               }
+          } else {
+               dessertsArray[targetId - 1].classList.add("current");
+               buttonChange();
+          }
+     },
+};
+
+const itemSelector = (itemParentId, itemId) => {
+     const menuEditing = menu[itemParentId];
+     menuEditing(itemId);
+};
+
+// trackMainCourses.addEventListener("click", (e) => {
+//      const currentMainCourse = trackMainCourses.querySelector(".current");
+//      if (currentMainCourse) {
+//           currentMainCourse.classList.remove("current");
+//      }
+//      let targetIndex = mainCoursesArray.indexOf(e.target);
+//      if (targetIndex != -1) {
+//           mainCoursesArray[targetIndex].classList.add("current");
+//      } else if (mainCourses.indexOf(e.target.parentElement) != -1) {
+//           targetIndex = mainCoursesArray.indexOf(e.target.parentElement);
+//           mainCoursesArray[targetIndex].classList.add("current");
+//      }
+//      order.mainCourse = mainCoursesArray[targetIndex].children[1].innerText;
+//      order.mainCoursePrice = Number(
+//           mainCoursesArray[targetIndex].children[3].innerText.replace(",", ".").replace("R$ ", "")
+//      );
+//      buttonChange();
+// });
+// trackBeverages.addEventListener("click", (e) => {
+//      const targetBeverages = trackBeverages.querySelector(".current");
+//      if (targetBeverages) {
+//           targetBeverages.classList.remove("current");
+//      }
+//      let targetIndex = beveragesArray.indexOf(e.target);
+//      if (targetIndex != -1) {
+//           beveragesArray[targetIndex].classList.add("current");
+//      } else if (beverages.indexOf(e.target.parentElement) != -1) {
+//           targetIndex = beveragesArray.indexOf(e.target.parentElement);
+//           beveragesArray[targetIndex].classList.add("current");
+//      }
+//      order.beverage = beveragesArray[targetIndex].children[1].innerText;
+//      order.beveragePrice = Number(
+//           beveragesArray[targetIndex].children[3].innerText.replace(",", ".").replace("R$ ", "")
+//      );
+//      buttonChange();
+// });
+// trackDesserts.addEventListener("click", (e) => {
+//      const currentDesserts = trackDesserts.querySelector(".current");
+//      if (currentDesserts) {
+//           currentDesserts.classList.remove("current");
+//      }
+//      let targetIndex = dessertsArray.indexOf(e.target);
+//      if (targetIndex != -1) {
+//           dessertsArray[targetIndex].classList.add("current");
+//      } else if (desserts.indexOf(e.target.parentElement) != -1) {
+//           targetIndex = dessertsArray.indexOf(e.target.parentElement);
+//           dessertsArray[targetIndex].classList.add("current");
+//      }
+//      order.dessert = dessertsArray[targetIndex].children[1].innerText;
+//      order.dessertPrice = Number(
+//           dessertsArray[targetIndex].children[3].innerText.replace(",", ".").replace("R$ ", "")
+//      );
+//      buttonChange();
+// });
 const buttonChange = () => {
      if (document.getElementsByClassName("current").length == 3) {
           document.querySelector(".notReady").style.display = "none";
           document.querySelector(".ready").style.display = "block";
+     } else {
+          document.querySelector(".notReady").style.display = "block";
+          document.querySelector(".ready").style.display = "none";
      }
 };
 const closure = () => {
