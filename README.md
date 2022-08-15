1451bda1decce41d2df8044cd883ae0d19822405
Miguel Domingos <migueldsp@hotmail.com>
2022-08-15 11:33:51 -0300

    orderEditing improvement + README

---
+++ README.md
@@ -0,0 +1,27 @@
+# **Projeto 3: DrivenEats**
+
+Esse projeto foi feito como parte da minha formação como Desenvolvedor Web Full stack na Driven. O objetivo foi implementar uma ferramente que facilitaria o recebimento de pedidos para um restaurante, utilizando um website que, atravez de uma API, encaminharia todos os dados necessários do cliente e o respectivo pedido para o WhatsApp do estabelecimento.
+
+## **Conteúdos exercitados**:
+
+-    Manipulação da DOM por meio de javascript puro (querySelector, getElementBy... )
+-    Event Handlers (principalmente onclick)
+-    encodeURIComponent e uso básico de APIs (WhatsApp's click to chat)
+-    Javascript básico (funções, if ) e melhores práticas
+
+## **Resultados**:
+
+Fiquei bastante satisfeito com o resultado pois, com uma boa gestão do tempo, consegui completar o projeto rapidamente. Isto me permitiu experimentar formas otimizadas de resolver o mesmo problema.
+
+![Imagem do resultado final](https://i.imgur.com/292Sv8o.png)
+![Imagem do resultado 2](https://i.imgur.com/pUiXTxt.png)
+
+Para mostrar o resultado, a aplicação foi colocada no ar no [Github Pages](https://migueldsp.github.io/projeto3-driveneats/)! Lembrando, para quem for abrir, que só foi implementada a versão mobile.
+
+## **Observações**:
+
+Como sobrou tempo após a execução do projeto, tentei adicionar recursos **_extras_**, tais como:
+
+-    Cards com itens sendo gerado de forma dinâmica, o que permitiu reduzir linhas repetitivas de HTML
+-    Melhor organização das funções, com seus escopos bem definidos e otimizados.
+-    Uso de variáveis no css para facilitar a futura manutenção do código
--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // New item template  (new categories can also be created)
-// '<div class="item" onclick="orderEditing(this.parentNode.id , this.id)" id="---ID---">\n<img src="---IMAGE-SOURCE---" />\n<h4 class="bolder">---NAME---</h4>\n<h5>---DESCRIPTION---</h5>\n<div><h4>---PRICE---</h4><ion-icon name="checkmark-circle"></ion-icon></div>\n</div>',
+// '<div class="item" onclick="orderEditing(this.parentNode.id , this)">\n<img src="---IMAGE-SOURCE---" />\n<h4 class="bolder">---NAME---</h4>\n<h5>---DESCRIPTION---</h5>\n<div><h4>---PRICE---</h4><ion-icon name="checkmark-circle"></ion-icon></div>\n</div>',
 const menuItems = {
      mainCourses: [
-          '<div class="item" onclick="orderEditing(this.parentNode.id , this.id)" id="1">\n<img src="./images/Strogonoff-Vegetariano-de-Palmito-1200x800.png" />\n<h4 class="bolder">Strogonoff Vegetariano</h4>\n<h5>Acompanha arroz e batata palha.</h5>\n<div><h4>R$ 24,90</h4><ion-icon name="checkmark-circle"></ion-icon></div>\n</div>',
-          '<div class="item" onclick="orderEditing(this.parentNode.id , this.id)" id="2">\n<img src="./images/omelete-4-queijos.jpg" />\n<h4 class="bolder">Omelete 4 Queijos</h4>\n<h5>Acompanha salada de alface e tomate.</h5>\n<div><h4>R$ 22,90</h4><ion-icon name="checkmark-circle"></ion-icon></div>\n</div>',
-          '<div class="item" onclick="orderEditing(this.parentNode.id , this.id)" id="3">\n<img src="./images/pizza-marguerita.jpg" />\n<h4 class="bolder">Pizza Marguerita</h4>\n<h5>Nossa deliciosa pizza brotinho</h5>\n<div><h4>R$ 23,90</h4><ion-icon name="checkmark-circle"></ion-icon></div>\n</div>',
+          '<div class="item" onclick="orderEditing(this.parentNode.id , this)">\n<img src="./images/Strogonoff-Vegetariano-de-Palmito-1200x800.png" />\n<h4 class="bolder">Strogonoff Vegetariano</h4>\n<h5>Acompanha arroz e batata palha.</h5>\n<div><h4>R$ 24,90</h4><ion-icon name="checkmark-circle"></ion-icon></div>\n</div>',
+          '<div class="item" onclick="orderEditing(this.parentNode.id , this)">\n<img src="./images/omelete-4-queijos.jpg" />\n<h4 class="bolder">Omelete 4 Queijos</h4>\n<h5>Acompanha salada de alface e tomate.</h5>\n<div><h4>R$ 22,90</h4><ion-icon name="checkmark-circle"></ion-icon></div>\n</div>',
+          '<div class="item" onclick="orderEditing(this.parentNode.id , this)">\n<img src="./images/pizza-marguerita.jpg" />\n<h4 class="bolder">Pizza Margherita</h4>\n<h5>Nossa deliciosa pizza brotinho</h5>\n<div><h4>R$ 23,90</h4><ion-icon name="checkmark-circle"></ion-icon></div>\n</div>',
      ],
      beverages: [
-          '<div class="item" onclick="orderEditing(this.parentNode.id , this.id)" id="1">\n<img src="./images/adesivo-parede-decoracao-suco-de-laranja-fruta-lanchonete-adesivo-decorativo.jpg" />\n<h4 class="bolder">Suco de Laranja</h4>\n<h5>Da fruta, docinha. Copo de 300ml.</h5>\n<div><h4>R$ 8,90</h4><ion-icon name="checkmark-circle"></ion-icon></div>\n</div>',
-          '<div class="item" onclick="orderEditing(this.parentNode.id , this.id)" id="2">\n<img src="./images/suco-uva-integral.jpeg" />\n<h4 class="bolder">Suco de Uva</h4>\n<h5>300ml de um delicioso suco integral</h5>\n<div><h4>R$ 9,90</h4><ion-icon name="checkmark-circle"></ion-icon></div>\n</div>',
-          '<div class="item" onclick="orderEditing(this.parentNode.id , this.id)" id="3">\n<img src="./images/coca-cola.jpeg" />\n<h4 class="bolder">Coca-Cola</h4>\n<h5>Lata 350ml</h5>\n<div><h4>R$ 5,90</h4><ion-icon name="checkmark-circle"></ion-icon></div>\n</div>',
+          '<div class="item" onclick="orderEditing(this.parentNode.id , this)">\n<img src="./images/adesivo-parede-decoracao-suco-de-laranja-fruta-lanchonete-adesivo-decorativo.jpg" />\n<h4 class="bolder">Suco de Laranja</h4>\n<h5>Da fruta, docinha. Copo de 300ml.</h5>\n<div><h4>R$ 8,90</h4><ion-icon name="checkmark-circle"></ion-icon></div>\n</div>',
+          '<div class="item" onclick="orderEditing(this.parentNode.id , this)">\n<img src="./images/suco-uva-integral.jpeg" />\n<h4 class="bolder">Suco de Uva</h4>\n<h5>300ml de um delicioso suco integral</h5>\n<div><h4>R$ 9,90</h4><ion-icon name="checkmark-circle"></ion-icon></div>\n</div>',
+          '<div class="item" onclick="orderEditing(this.parentNode.id , this)">\n<img src="./images/coca-cola.jpeg" />\n<h4 class="bolder">Coca-Cola</h4>\n<h5>Lata 350ml</h5>\n<div><h4>R$ 5,90</h4><ion-icon name="checkmark-circle"></ion-icon></div>\n</div>',
      ],
      desserts: [
-          '<div class="item" onclick="orderEditing(this.parentNode.id , this.id)" id="1">\n<img src="./images/sorvete-de-flocos.png" />\n<h4 class="bolder">Sorvete de Flocos</h4>\n<h5>3 bolas do melhor sabor já inventado.</h5>\n<div><h4>R$ 9,90</h4><ion-icon name="checkmark-circle"></ion-icon></div>\n</div>',
-          '<div class="item" onclick="orderEditing(this.parentNode.id , this.id)" id="2">\n<img src="./images/mousse-maracuja.png" />\n<h4 class="bolder">Mousse de Maracujá</h4>\n<h5>Amor em forma de pote?</h5>\n<div><h4>R$ 8,90</h4><ion-icon name="checkmark-circle"></ion-icon></div>\n</div>',
-          '<div class="item" onclick="orderEditing(this.parentNode.id , this.id)" id="3">\n<img src="./images/pudim.jpg" />\n<h4 class="bolder">Pudim</h4>\n<h5>Apenas um belo e lustroso pudim.</h5>\n<div><h4>R$ 7,90</h4><ion-icon name="checkmark-circle"></ion-icon></div>\n</div>',
+          '<div class="item" onclick="orderEditing(this.parentNode.id , this)">\n<img src="./images/sorvete-de-flocos.png" />\n<h4 class="bolder">Sorvete de Flocos</h4>\n<h5>3 bolas do melhor sabor já inventado.</h5>\n<div><h4>R$ 9,90</h4><ion-icon name="checkmark-circle"></ion-icon></div>\n</div>',
+          '<div class="item" onclick="orderEditing(this.parentNode.id , this)">\n<img src="./images/mousse-maracuja.png" />\n<h4 class="bolder">Mousse de Maracujá</h4>\n<h5>Amor em forma de pote?</h5>\n<div><h4>R$ 8,90</h4><ion-icon name="checkmark-circle"></ion-icon></div>\n</div>',
+          '<div class="item" onclick="orderEditing(this.parentNode.id , this)">\n<img src="./images/pudim.jpg" />\n<h4 class="bolder">Pudim</h4>\n<h5>Apenas um belo e lustroso pudim.</h5>\n<div><h4>R$ 7,90</h4><ion-icon name="checkmark-circle"></ion-icon></div>\n</div>',
      ],
 };
 // COLOCA OS ELEMENTOS DE TODAS AS CATEGORIAS NO MESMO OBJETO
@@ -33,33 +33,19 @@ Object.values(menuItems).map((category, i) => {
 // OBJETO QUE GUARDARÁ TODAS AS INFOS DE PEDIDOS
 const order = [];
 order.cellPhone = "+5555555555555";
-// COLOCA OS ARRAYS DE TODAS AS CATEGORIAS EM UM OBJETO
-const menuArrays = {
-     mainCourses: Array.from(trackMenu.mainCourses.children),
-     beverages: Array.from(trackMenu.beverages.children),
-     desserts: Array.from(trackMenu.desserts.children),
-};
 // COM INFOS DO CLICK FAZ A MOVIMENTAÇÃO DA CLASSE "current",
 // QUE É RESPONSÁVEL PELA BORDA VERDE NOS ITENS SELECIONADOS
-const orderEditing = (targetParentId, targetId) => {
-     const targetArray = Array.from(menuArrays[targetParentId]);
+const orderEditing = (targetParentId, target) => {
      const currentSelection = trackMenu[targetParentId].querySelector(".current");
-     if (currentSelection) {
-          const currentId = targetArray.indexOf(currentSelection);
-          if (currentId === targetId - 1) {
-               targetArray[targetId - 1].classList.remove("current");
-          } else {
-               targetArray[currentId].classList.remove("current");
-               targetArray[targetId - 1].classList.add("current");
-          }
-     } else {
-          targetArray[targetId - 1].classList.add("current");
+     target.classList.toggle("current");
+     if (currentSelection && currentSelection !== target) {
+          currentSelection.classList.remove("current");
      }
      buttonChange();
      // ATRIBUIÇÃO DAS CARACTERÍSTICAS DO ITEM CLICADO AO OBJETO "order"
-     order[targetParentId] = targetArray[targetId - 1].children[1].innerText;
+     order[targetParentId] = target.children[1].innerText;
      order[targetParentId + "Price"] = Number(
-          targetArray[targetId - 1].children[3].innerText.replace(",", ".").replace("R$ ", "")
+          target.children[3].innerText.replace(",", ".").replace("R$ ", "")
      );
 };
 // RESPONSÁVEL POR LIGAR E DESLIGAR O BOTÃO PARA FINALIZAR PEDIDO
@@ -72,11 +58,11 @@ const buttonChange = () => {
           document.querySelector(".ready").style.display = "none";
      }
 };
-// FINALIZA O PEDINDO LIGANDO A TELA FINAL COM TODAS AS INFOS PARA CONFIRMAÇÃO DO USUÁRIO
-// ALÉM DE MONTAR O TEXTO QUE SERÁ ENVIADO POR WHATSAPP
+// FINALIZA O PEDINDO LIGANDO A TELA FINAL COM TODAS AS INFOS PARA CONFIRMAÇÃO DO USUÁRIO,
+// ALÉM DE MONTAR O TEXTO QUE SERÁ ENVIADO POR WHATSAPP.
 const closure = () => {
      const name = prompt("Insira seu nome");
-     const adress = prompt("Agora, seu endereço");
+     const address = prompt("Agora, seu endereço");
      order.total = order.dessertsPrice + order.beveragesPrice + order.mainCoursesPrice;
      const closureTxt = document.querySelector(".closure-txt").children;
      closureTxt[3].children[0].innerText = order.mainCourses;
@@ -87,7 +73,7 @@ const closure = () => {
      closureTxt[7].children[1].innerText = String(order.dessertsPrice).replace(".", ",") + "0";
      closureTxt[9].children[1].innerText = "R$ " + String(order.total).replace(".", ",") + "0";
      closureTxt[12].innerText = "Nome: " + name;
-     closureTxt[14].innerText = "Endereço: " + adress;
+     closureTxt[14].innerText = "Endereço: " + address;
 
      document.querySelector(".closure").style.display = "grid";
 
@@ -103,7 +89,7 @@ const closure = () => {
                "0\n\nNome:  " +
                name +
                "\nEndereço:  " +
-               adress
+               address
      );
 };
 const send = () => {
